164f5adc5e0b2ac255921f8d819941e9b33f4ff8
adityasharma2020 <aditya15116617@gmail.com>
2024-06-17 00:49:16 +0530

    stripe webhook setup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
 import Link from 'next/link';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import { buttonVariants } from './ui/button';
-import  {LoginLink, RegisterLink}  from '@kinde-oss/kinde-auth-nextjs/server';
+import {
+	LoginLink,
+	RegisterLink,
+	getKindeServerSession,
+} from '@kinde-oss/kinde-auth-nextjs/server';
 import { ArrowRight } from 'lucide-react';
+import UserAccountNav from './UserAccountNav';
 
-const Navbar = () => {
+const Navbar = async () => {
+	const { getUser } = getKindeServerSession();
+	const user = await getUser();
 	return (
 		<nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
 			<MaxWidthWrapper>
@@ -16,34 +23,58 @@ const Navbar = () => {
 					{/* TODO: add mobile navbar */}
 
 					<div className='hidden items-center space-x-4 sm:flex'>
-						<>
-							<Link
-								className={buttonVariants({
-									variant: 'ghost',
-									size: 'sm',
-								})}
-								href='/pricing'
-							>
-								Pricing
-							</Link>
+						{!user ? (
+							<>
+								<Link
+									className={buttonVariants({
+										variant: 'ghost',
+										size: 'sm',
+									})}
+									href='/pricing'
+								>
+									Pricing
+								</Link>
 
-							<LoginLink
-								className={buttonVariants({
-									variant: 'ghost',
-									size: 'sm',
-								})}
-							>
-								Sign In
-							</LoginLink>
+								<LoginLink
+									className={buttonVariants({
+										variant: 'ghost',
+										size: 'sm',
+									})}
+								>
+									Sign In
+								</LoginLink>
 
-                            <RegisterLink
-								className={buttonVariants({
-									size: 'sm',
-								})}
-							>
-								Get started <ArrowRight className=' '></ArrowRight>
-							</RegisterLink>
-						</>
+								<RegisterLink
+									className={buttonVariants({
+										size: 'sm',
+									})}
+								>
+									Get started <ArrowRight className=' '></ArrowRight>
+								</RegisterLink>
+							</>
+						) : (
+							<>
+								<Link
+									className={buttonVariants({
+										variant: 'ghost',
+										size: 'sm',
+									})}
+									href='/dashboard'
+								>
+									Dashboard
+								</Link>
+
+								<UserAccountNav
+									email={user.email || ''}
+									imageUrl={user.picture || ''}
+									name={
+										!user.given_name || !user.family_name
+											? 'Your Account'
+											: `${user.given_name} ${user.family_name}`
+									}
+								/>
+							</>
+						)}
 					</div>
 				</div>
 			</MaxWidthWrapper>
