1e328d4e20619fd107fe070884cea258277bb24e
adityasharma2020 <aditya15116617@gmail.com>
2024-06-20 04:28:40 +0530

    stripe absolute url fixed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
 	if (typeof window !== 'undefined') return path;
-	if (process.env.VERCEL_URL) return `https://quill-pdf-chat.vercel.app/${path}`;
+	if (process.env.VERCEL_URL) return `https://quill-pdf-chat.vercel.app${path}`;
 	return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
 export function constructMetadata({
